3bcd0e95777dc48154c95d52ae44d4f7063746f3
HyeonJeong <58041212+HyeonJeong519@users.noreply.github.com>
2023-03-07 01:08:05 +0900

    3/7

---
+++ src/BJ_1926.java
@@ -0,0 +1,34 @@
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BJ_1926 {
+    static Queue<Point> queue = new LinkedList<Point>();
+
+    static void bfs(int[][] array){
+        while (!queue.isEmpty()){
+
+        }
+
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] array = new int[n][m];
+        // 배열 입력
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine()," ");
+            for (int j = 0; j < m; j++) {
+                array[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+    }
+}
